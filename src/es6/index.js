3c3009c6c7ef77b39733523ccf20a47d156b0f3b
imsourceboom <ecmascript.developer@gmail.com>
2019-04-01 10:54:56 +0900

    http redirect https

--- src/es6/index.js
+++ src/es6/index.js
@@ -5,3 +5,7 @@ import './header';
 import './components/detail';
 import './components/result';
 import './admin';
+
+if (document.location.protocol == 'http:') {
+    document.location.href = document.location.href.replace('http:', 'https:');
+}
